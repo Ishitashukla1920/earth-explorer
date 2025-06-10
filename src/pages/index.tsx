c4077588aadfc214e2c54c8c683385606387c6a4
Trando_tech <68798607+trandoin@users.noreply.github.com>
2025-06-10 22:25:36 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -449,7 +449,7 @@ const TeamPage = ({
 }: TeamPageProps) => {
 
   return (
-    <div className="min-h-screen bg-black text-white relative overflow-hidden " style={{ fontFamily: 'Radley' }}>
+    <div className="min-h-screen bg-black text-white relative overflow-hidden standard-container">
       {/* Top to Bottom Overlay for Team Page */}
       <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black/30 pointer-events-none z-5"></div>
 
