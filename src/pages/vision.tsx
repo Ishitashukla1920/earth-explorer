7918584ef9c5c111a6d8f5dba793f625d0d5b45c
Ishita Shukla <ishita20jan@gmail.com>
2025-06-13 18:56:39 +0530

    new fix

--- src/pages/vision.tsx
+++ src/pages/vision.tsx
@@ -47,7 +47,7 @@ const VisionPage = () => {
             width={800}
             height={800}
             className="opacity-100  absolute "
-            style={{ left:'159%',top: '15%', right: '0% !important' , zIndex: 0 }}
+            style={{ left:'17rem',top: '15%', right: '0% !important' , zIndex: 0 }}
 
           />
     </div>
